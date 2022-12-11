b195976c38921f00a3c94a4220faae7cee02da8f
Przemyslaw Plata <paplata50@gmail.com>
2022-12-11 21:26:42 +0100

    commit

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:kwakwa5!@localhost/height_collector"
+db = SQLAlchemy(app)
+db.init_app(app)
 
-class Data(sqlalchemy)
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_=db.Column(db.Integer)
+    
+    def __init__(self,email_, height_):
+        self.email_ = email_
+        self.height_ = height_
+    
 
 @app.route("/")
 def index():
@@ -16,10 +30,16 @@ def success():
     if request.method == "POST":
         email = request.form["email_name"]
         height = request.form["height_name"]
-        print(request.form)
-        return render_template("success.html")
-
+        if not db.session.query(Data).filter(Data.email_== email).count():
+            data=Data(email,height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        return render_template("index.html",
+                               text="That email already exists.")
 
 if __name__ == "__main__":
     app.debug = True
+    with app.app_context():
+        db.create_all()
     app.run()
